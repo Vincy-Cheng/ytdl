b4b883cd7936ab46db46fcbec853d0b5ffce50a6
Vincy-Cheng <60846680+Vincy-Cheng@users.noreply.github.com>
2023-07-09 15:46:51 +0800

    re-contruct the folders

--- python/convert.py
+++ python/convert.py
@@ -14,14 +14,14 @@ url = "https://www.youtube.com/watch?v=ShP6KqZVQtM"
 target_path = "./downloads"
 print("Start converting")
 yt = YouTube(url)
-
+print('ytdl')
 video = yt.streams.filter(only_audio=True).first()
-
+print('convert to stream')
 out_file = video.download(output_path=target_path)
-
+print('out file')
 base, ext = os.path.splitext(out_file)
-# new_file = base + '.m4a'
-# os.rename(out_file, new_file)
+new_file = base + '.m4a'
+os.rename(out_file, new_file)
 
 print("target path = " + (new_file))
 print("Audio has been successfully downloaded.")
